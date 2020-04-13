238237b71320cb3b21b8e93caf0ad256c9fb3e02
Adler Luders <adlerluders@gmail.com>
2020-04-13 12:28:10 -0400

    Updated Card Details component to display information on specific country

--- src/components/CardsDetails/CardsDetails.jsx
+++ src/components/CardsDetails/CardsDetails.jsx
@@ -29,26 +29,63 @@ class CardsDetails extends React.Component {
       subregion,
       capital,
       topLevelDomain,
-      // currencies,
-      // languages,
+      currencies,
+      languages,
       // borders,
       flag,
     } = this.state.countries;
 
     // work on being able to display currencies
 
+    // const currencies = this.state.countries.currencies;
+
+    // if (this.state.countries.currencies) {
+    //   console.log(currencies[0].code);
+    // }
+
     return (
-      <div>
+      <div className="Cards-Details">
         <Link to="/">Back to Home</Link>
         <h1>This is Cards Details {this.props.match.params.cardId}</h1>
-        <p>{name}</p>
-        <p>{nativeName}</p>
-        <p>{population}</p>
-        <p>{region}</p>
-        <p>{subregion}</p>
-        <p>{capital}</p>
-        <p>{topLevelDomain}</p>
-        <p>{flag}</p>
+
+        <div className="Details-Info">
+          <div className="Detail-Flag">
+            <img src={flag} alt={name + "flag"} />
+          </div>
+          <div>
+            <h2> {name} </h2>
+            <p>
+              Native Name: <span> {nativeName} </span>
+            </p>
+            <p>
+              Population: <span> {population} </span>
+            </p>
+            <p>
+              Region: <span> {region} </span>
+            </p>
+            <p>
+              Sub Region: <span> {subregion} </span>
+            </p>
+            <p>
+              Capital: <span> {capital} </span>
+            </p>
+          </div>
+          <div>
+            <p>
+              Top Level Domain: <span>{topLevelDomain}</span>
+            </p>
+            <p>
+              Currency: <span>{currencies ? currencies[0].code : null}</span>
+            </p>
+            <p>
+              Languages:
+              <span>
+                {languages ? languages.map((lan) => `${lan.name},`) : null}
+              </span>
+            </p>
+          </div>
+          <div>Info Sec</div>
+        </div>
       </div>
     );
   }
