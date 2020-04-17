f4c59be7c8f95d0bb9ccb5aaad29f285197b5da8
Adler Luders <adlerluders@gmail.com>
2020-04-17 13:07:48 -0400

    Updated app with hooks

--- src/components/CardsDetails/CardsDetails.jsx
+++ src/components/CardsDetails/CardsDetails.jsx
@@ -1,139 +1,108 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { withRouter, Link } from "react-router-dom";
 
 import "./CardsDetails.css";
 
-class CardsDetails extends React.Component {
-  constructor(props) {
-    super(props);
+const CardsDetails = ({ match, history }) => {
+  const [countries, setCountries] = useState([]);
 
-    this.state = {
-      countries: [],
-    };
-
-    // this.separator = this.separator.bind(this);
-  }
-
-  history = this.props;
-
-  fetchCountry = (countryCode) => {
+  const fetchCountry = (countryCode) => {
     fetch(`https://restcountries.eu/rest/v2/alpha/${countryCode}`)
       .then((res) => res.json())
-      .then((country) => this.setState({ countries: country }));
+      .then((country) => setCountries(country));
   };
 
-  componentDidMount() {
-    this.fetchCountry(this.props.match.params.cardId);
-  }
+  useEffect(() => {
+    fetchCountry(match.params.cardId);
+  });
 
-  render() {
-    const {
-      name,
-      nativeName,
-      population,
-      region,
-      subregion,
-      capital,
-      topLevelDomain,
-      currencies,
-      languages,
-      borders,
-      flag,
-    } = this.state.countries;
+  const {
+    name,
+    nativeName,
+    population,
+    region,
+    subregion,
+    capital,
+    topLevelDomain,
+    currencies,
+    languages,
+    borders,
+    flag,
+  } = countries;
 
-    return (
-      <div className="Cards-Details">
-        <div id="Back-Section">
-          <Link to="/" className="Back">
-            <ion-icon name="arrow-back-outline" id="Back-Icon"></ion-icon>
-            Back
-          </Link>
+  return (
+    <div className="Cards-Details">
+      <div id="Back-Section">
+        <Link to="/" className="Back">
+          {" "}
+          <ion-icon name="arrow-back-outline" id="Back-Icon"></ion-icon>
+          Back
+        </Link>
+      </div>
+
+      <div className="Details">
+        <div className="Details-Flag">
+          <img src={flag} alt={name + "flag"} />
         </div>
 
-        <div className="Details">
-          <div className="Details-Flag">
-            <img src={flag} alt={name + "flag"} />
+        <div className="Details-Info">
+          <h2>{name}</h2>
+          <div className="Name-Details">
+            <p>
+              Native Name: <span> {nativeName} </span>
+            </p>
+            <p>
+              Population:
+              <span>{population && population.toLocaleString()}</span>
+            </p>
+            <p>
+              Region: <span>{region}</span>
+            </p>
+            <p>
+              Sub Region: <span>{subregion}</span>
+            </p>
+            <p>
+              Capital: <span>{capital}</span>
+            </p>
           </div>
 
-          <div className="Details-Info">
-            <h2> {name} </h2>
-            <div className="Name-Details">
-              <p>
-                Native Name: <span> {nativeName} </span>
-              </p>
-              <p>
-                Population:
-                <span> {population && population.toLocaleString()} </span>
-              </p>
-              <p>
-                Region: <span> {region} </span>
-              </p>
-              <p>
-                Sub Region: <span> {subregion} </span>
-              </p>
-              <p>
-                Capital: <span> {capital} </span>
-              </p>
-            </div>
+          <div className="Other-Details">
+            <p>
+              Top Level Domain: <span>{topLevelDomain}</span>
+            </p>
+            <p>
+              Currency: <span>{currencies ? currencies[0].code : null}</span>
+            </p>
+            <p>
+              Languages:
+              <span>
+                {languages ? languages.map((lan) => `${lan.name},`) : null}
+              </span>
+            </p>
+          </div>
 
-            <div className="Other-Details">
-              <p>
-                Top Level Domain: <span>{topLevelDomain}</span>
-              </p>
-              <p>
-                Currency: <span>{currencies ? currencies[0].code : null}</span>
-              </p>
-              <p>
-                Languages:
-                <span>
-                  {languages ? languages.map((lan) => `${lan.name},`) : null}
-                </span>
-              </p>
-            </div>
-
-            <div className="Borders">
-              Borders:
-              {/*
-              {borders && borders !== 0 ? (
-                borders.map((border) => (
+          <div className="Borders">
+            Borders:
+            {borders
+              ? borders.map((border) => (
                   <button
                     key={border}
                     className="Border"
                     onClick={() => {
-                      this.fetchCountry(border);
-                      this.props.history.push(`/card-detail/${border}`);
+                      fetchCountry(border);
+                      history.push(`/card-detail/${border}`);
                     }}
                   >
                     {border}
                   </button>
                 ))
-              ) : (
-                <button>None</button>
-              )}
-              
-                if there are borders, map over and display a button, if not return a paragraph with the word none
-                */}
-              {borders
-                ? borders.map((border) => (
-                    <button
-                      key={border}
-                      className="Border"
-                      onClick={() => {
-                        this.fetchCountry(border);
-                        this.props.history.push(`/card-detail/${border}`);
-                      }}
-                    >
-                      {border}
-                    </button>
-                  ))
-                : null}
-            </div>
+              : null}
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default withRouter(CardsDetails);
